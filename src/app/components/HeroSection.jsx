9dfda07c9904998a5c7568dcb170c41881cf8757
Allen Guo <allenguo166@gmail.com>
2024-09-14 01:16:10 -0700

    Initialize the Navbar Component

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,20 +1,41 @@
+"use client"
 import React from 'react';
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return (
     <section>
       <div className='grid grid-cols-1 lg:grid-cols-12'>
-        <div className='col-span-7 place-self-center'>
+        <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Hello, I'm Allen
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+              Hello, I'm{" "}
+            </span>
+            <TypeAnimation
+              sequence={[
+                'Allen',
+                1000,
+                'Web & Mobile Developer',
+                1000,
+                'Backend Developer',
+                1000,
+                'Data Analyst',
+                1000
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
+          <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
             Interested in building robust and scalable system using any technical stack. Nice to meet you!
           </p>
           <div>
-            <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
-            <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 bg-white hover:bg-slate-200 text-white'>Hire Me</button>
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
+              <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+            </button>
           </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
