2b5c04271a84b84029595d65abb77c9a95621815
Allen Guo <allenguo166@gmail.com>
2024-09-13 23:46:56 -0700

    Create the first component

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -9,9 +9,13 @@ const HeroSection = () => {
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
             Hello, I'm Allen
           </h1>
-          <p className='text-[#ADB7BE] text-lg lg:text-xl'>
-            Interested in building robust and scalable system. Nice to meet you!
+          <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
+            Interested in building robust and scalable system using any technical stack. Nice to meet you!
           </p>
+          <div>
+            <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
+            <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
+          </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
           <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
