ba7fd9b641a4d29ab07e17a632245827c9314de4
Allen Guo <allenguo166@gmail.com>
2024-09-18 17:37:02 -0700

    Bind the domain to enable email sending

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -3,15 +3,15 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROMEMAIL;
+const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req) {
-  const { body } = await req.json();
-  const { email, subject, message } = body;
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ['rui762432@gmail.com', email],
+      to: [email],
       subject: subject,
       react: (
         <>
